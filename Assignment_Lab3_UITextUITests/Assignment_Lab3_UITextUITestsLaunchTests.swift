a7c809f7429f0ddeda2a1a97e77fe2e79c2a7535
Darshil1001 <darshilpatel11001@gmail.com>
2024-01-28 16:00:18 -0500

    Initial Commit

---
+++ Assignment_Lab3_UITextUITests/Assignment_Lab3_UITextUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Assignment_Lab3_UITextUITestsLaunchTests.swift
+//  Assignment_Lab3_UITextUITests
+//
+//  Created by user238116 on 1/23/24.
+//
+
+import XCTest
+
+final class Assignment_Lab3_UITextUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
